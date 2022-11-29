2e5b20e337dbc7f6a3b62d6506a997676e592561
DEV <96439384+DEV-SHUKLA-GITHUB@users.noreply.github.com>
2022-11-29 20:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ process.on("uncaughtException", (err) => {
   console.error(`There was an uncaught error ${err}`);
   process.exit(1);
 });
-// fileSystem.readFile(filePath, "utf8", (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
-// fileSystem.appendFile(filePath, " for fruits", (err) => {
-//   if (!err) console.log("file is updated");
-// });
-// fileSystem.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
-//   if (!err) console.log("file name is updated");
-// });
+fileSystem.readFile(filePath, "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+fileSystem.appendFile(filePath, " for fruits", (err) => {
+  if (!err) console.log("file is updated");
+});
+fileSystem.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+  if (!err) console.log("file name is updated");
+});
 
-// fileSystem.unlinkSync(`${dirPath}/fruit.txt`);
+fileSystem.unlinkSync(`${dirPath}/fruit.txt`);
